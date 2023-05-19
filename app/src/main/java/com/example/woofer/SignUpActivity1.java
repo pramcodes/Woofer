950fc2ba36e48a3adb0617acd31fc7946ec013e2
LaaiqahR <laaiqahravat0709@gmail.com>
2023-05-19 22:05:23 +0200

    Date picker done

--- app/src/main/java/com/example/woofer/SignUpActivity1.java
+++ app/src/main/java/com/example/woofer/SignUpActivity1.java
@@ -1,19 +1,76 @@
 package com.example.woofer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SignUpActivity1 extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity1";
+    private TextView displayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up1);
+        //setContentView(R.layout.activity_sign_up1);
+        setContentView(R.layout.activity_sign_in_test);
 
-        SignUpActivity1.this.runOnUiThread(new Runnable() {
+        //for datePicker
+        displayDate= (TextView) findViewById(R.id.textViewDate);
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar= Calendar.getInstance();
+                int year= calendar.get(Calendar.YEAR);
+                int month= calendar.get(Calendar.MONTH);
+                int day= calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePicker= new DatePickerDialog(
+                        SignUpActivity1.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        year,month,day);
+                datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePicker.show();
+            }
+        });
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month= month+1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
+                String date=  month + "/" + day + "/" + year;
+                displayDate.setText(date);
+            }
+        };
+
+        Button backButton = (Button)findViewById(R.id.newAccBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity1.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+//Liam's code
+/*
+SignUpActivity1.this.runOnUiThread(new Runnable() {
             @Override
             public void run()
             {
@@ -36,5 +93,4 @@ public class SignUpActivity1 extends AppCompatActivity {
 
             }
         });
-    }
-}
+ */
---
+++ app/src/main/java/com/example/woofer/SignUpScroll.java
@@ -0,0 +1,5 @@
+package com.example.woofer;
+
+public class SignUpScroll {
+
+}
