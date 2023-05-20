bed292deac9e538d6a5dfd27017c7a25a26a785d
LaaiqahR <laaiqahravat0709@gmail.com>
2023-05-20 14:35:04 +0200

    Calender

--- app/src/main/java/com/example/woofer/SignUpActivity1.java
+++ app/src/main/java/com/example/woofer/SignUpActivity1.java
@@ -11,7 +11,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
@@ -20,6 +24,12 @@ public class SignUpActivity1 extends AppCompatActivity {
     private static final String TAG = "SignUpActivity1";
     private TextView displayDate;
     private DatePickerDialog.OnDateSetListener dateSetListener;
+
+    EditText username, fName, lName, email;
+    Button confirm;
+    TextView date;
+    TextInputLayout password, confirmPassword;
+    private boolean isBirthdateSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +65,7 @@ public class SignUpActivity1 extends AppCompatActivity {
                 displayDate.setText(date);
             }
         };
-
+        //Return to login page
         Button backButton = (Button)findViewById(R.id.newAccBackButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +74,55 @@ public class SignUpActivity1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Validation checks
+        username=findViewById(R.id.uNameNewAccView);
+        fName=findViewById(R.id.fNameNewAccView);
+        lName=findViewById(R.id.lNameNewAccView);
+        email=findViewById(R.id.editTextEmail);
+        confirm= findViewById(R.id.newAccConfirmButton);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.length()==0){
+                    username.setError("Enter username");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (fName.length()==0) {
+                    fName.setError("Enter first name");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (lName.length()==0) {
+                    lName.setError("Enter last name");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (email.length()==0) {
+                    email.setError("Enter email");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                /*
+                 else if (!isBirthdateSelected) {
+                    displayDate.setError("Select your birthdate");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+
+                 else if (password.getEditText().length() == 0) {
+                    password.setError("Enter password");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (confirmPassword.getEditText().length() == 0) {
+                    confirmPassword.setError("Enter password");
+                    Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
+                }
+                 */
+
+                else {
+                    Toast.makeText(SignUpActivity1.this, "Works :D", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 }
