29370ebcc9aa23e1560a6e4a2fa8f8c0126692be
LaaiqahR <laaiqahravat0709@gmail.com>
2023-05-30 16:14:25 +0200

    created and did design for userAddWoof activity.

--- app/src/main/java/com/example/woofer/User.java
+++ app/src/main/java/com/example/woofer/User.java
@@ -1,19 +1,32 @@
 package com.example.woofer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class User extends AppCompatActivity {
+
+    private ImageView objectImageView;
+    private static final int PICK_IMAGE_REQUEST=100;
+    private Uri imageFilePath;
+    private Bitmap imageToStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // Initialize the objectImageView
+        objectImageView = findViewById(R.id.profile_image);
         User.this.runOnUiThread(new Runnable() {
             @Override
             public void run()
@@ -29,5 +42,30 @@ public class User extends AppCompatActivity {
 
             }
         });
+    }
+    // profile picture things
+    public void chooseImage(View objectView) {
+        try {
+            Intent objectIntent = new Intent();
+            objectIntent.setType("image/*");
+            objectIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(objectIntent, PICK_IMAGE_REQUEST);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        try {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+                imageFilePath = data.getData();
+                imageToStore = MediaStore.Images.Media.getBitmap(getContentResolver(), imageFilePath);
+                objectImageView.setImageBitmap(imageToStore);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
