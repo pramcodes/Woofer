a247f0dcfc6fa56718b2b136cbc04b164309cf1d
zainab <zlorgat1007@gmail.com>
2023-05-30 12:56:32 +0200

    likes page

---
+++ app/src/main/java/com/example/woofer/likesPage.java
@@ -0,0 +1,15 @@
+package com.example.woofer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class likesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_likes_page);
+    }
+
+}
