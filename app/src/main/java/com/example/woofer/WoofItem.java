4a378e640fe23645e71917c96b359b090fc492a9
zainab <zlorgat1007@gmail.com>
2023-06-02 11:42:34 +0200

    likes page

--- app/src/main/java/com/example/woofer/WoofItem.java
+++ app/src/main/java/com/example/woofer/WoofItem.java
@@ -1,16 +1,22 @@
 package com.example.woofer;
 
 public class WoofItem {
+    private int likesCount;
+
+    private boolean isLiked;
     private String userId;
     private String timestamp;
     private String howl;
+
 
     public WoofItem(String userId, String timestamp, String howl) {
         this.userId = userId;
         this.timestamp = timestamp;
         this.howl = howl;
     }
-
+    public WoofItem(String howl) {
+        this.howl = howl;
+    }
     public String getUserId() {
         return userId;
     }
@@ -22,4 +28,21 @@ public class WoofItem {
     public String getHowl() {
         return howl;
     }
-}
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
+    }
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    }
+
--- app/src/main/java/com/example/woofer/likesPage.java
+++ app/src/main/java/com/example/woofer/likesPage.java
@@ -1,15 +1,35 @@
 package com.example.woofer;
-
+import com.example.woofer.PostAdapter;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class likesPage extends AppCompatActivity {
 
-    @Override
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<WoofItem> postList;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_likes_page);
-    }
 
-}
+        recyclerView = findViewById(R.id.likes_recycler_view);
+        postAdapter = new PostAdapter(postList);
+        postList = new ArrayList<>();
+        postList.add(new WoofItem("uoioijolio"));
+        postList.add(new WoofItem("111111112"));
+
+        recyclerView = findViewById(R.id.likes_recycler_view);
+        postAdapter = new PostAdapter(postList);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(postAdapter);
+    }}
