932d09a5079986e7943fc28a8629858666b85501
zainab <zlorgat1007@gmail.com>
2023-06-03 23:00:03 +0200

    more validations

--- app/src/main/java/com/example/woofer/SignUpActivity1.java
+++ app/src/main/java/com/example/woofer/SignUpActivity1.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -118,91 +119,128 @@ public class SignUpActivity1 extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String firstNameText = fName.getText().toString();
+                String lastNameText = lName.getText().toString();
+                String emailtext = email.getText().toString();
                 String Uname = username.getText().toString();
                 String Resp = CheckName(Uname);
-                if (Resp.equals("Username already exists")){
-                    Toast.makeText(SignUpActivity1.this,"Username taken, please try again", Toast.LENGTH_SHORT).show();
-                    username.setError("Choose a different Username");
-                }
-                else if (Resp.equals("Username does not exist")){
-                    Flag = true;
-                }
-
                 String password = passwordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
 
-                if (username.length()==0){
+                if (Resp.equals("Username already exists")) {
+                    Toast.makeText(SignUpActivity1.this, "Username taken, please try again", Toast.LENGTH_SHORT).show();
+                    username.setError("Choose a different Username");
+                } else if (Resp.equals("Username does not exist")) {
+                    Flag = true;
+                }
+
+
+                if (username.length() == 0) {
                     username.setError("Enter username");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
                     Flag = false;
-                }
-                else if (fName.length()==0) {
+                } else if (Uname.length() < 4) {// Check if the username is at least 4 characters long
+                    username.setError("Username must be at least 4 characters long");
+                    Toast.makeText(SignUpActivity1.this, "Username must be at least 4 characters long", Toast.LENGTH_SHORT).show();
+
+                } else if (!Uname.matches("[a-zA-Z0-9_]+")) { // Check if the username contains only alphanumeric characters and underscores
+                    username.setError("Username can only contain letters, numbers, and underscores");
+                    Toast.makeText(SignUpActivity1.this, "Username can only contain letters, numbers, and underscores", Toast.LENGTH_SHORT).show();
+                } else if (fName.length() == 0) {
                     fName.setError("Enter first name");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
                     Flag = false;
-                }
-                else if (lName.length()==0) {
+                } else if (lName.length() == 0) {
                     lName.setError("Enter last name");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
                     Flag = false;
-                }
-                else if (email.length()==0) {
+                } else if (!firstNameText.matches("[a-zA-Z]+")) {
+                    fName.setError("Enter a valid first name");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid first name", Toast.LENGTH_SHORT).show();
+                    // Check if the first name contains only letters
+                } else if (!lastNameText.matches("[a-zA-Z]+")) {
+                    lName.setError("Enter a valid last name");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid last name", Toast.LENGTH_SHORT).show();
+                    // Check if the last name contains only letters
+                } else if (email.length() == 0) {
                     email.setError("Enter email");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
                     Flag = false;
-                }
-                else if (!isDateSelected) {
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(emailtext).matches()) { //email is valid format
+                    email.setError("Enter email");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                } else if (!isDateSelected) {
                     // Date is not selected, show error message
                     displayDate.setError("Please select a date");
                     Flag = false;
-                }
-                else if (!password.equals(confirmPassword)) {
-                    confirmPasswordLayout.setError("Passwords do not match");
-                    Flag = false;
-                }
-                else if (password.length() < 8) {
-                    passwordLayout.setError("Password must be greater than 8 characters");
-                    Flag = false;
-                }
+                } else {
+                    // Age check
+                    Calendar calendar = Calendar.getInstance();
+                    int currentYear = calendar.get(Calendar.YEAR);
+                    int currentMonth = calendar.get(Calendar.MONTH) + 1;
+                    int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
 
-                else if (Flag == true){
-                    Toast.makeText(SignUpActivity1.this, "Works :D", Toast.LENGTH_SHORT).show();
-                    username.setError(null);
-                    fName.setError(null);
-                    lName.setError(null);
-                    email.setError(null);
-                    displayDate.setError(null);
-                    confirmPasswordLayout.setError(null);
-                    passwordLayout.setError(null);
-                    Flag = true;
+                    String[] selectedDateParts = displayDate.getText().toString().split("/");
+                    int selectedMonth = Integer.parseInt(selectedDateParts[0]);
+                    int selectedDay = Integer.parseInt(selectedDateParts[1]);
+                    int selectedYear = Integer.parseInt(selectedDateParts[2]);
 
+                    int age = currentYear - selectedYear;
 
-                    JSONObject obj = new JSONObject();
-                    try{
-                        obj.put("username",username.getText().toString());
-                        obj.put("fname",fName.getText().toString());
-                        obj.put("lname",lName.getText().toString());
-                        obj.put("password",password);
-                        obj.put("email",email.getText().toString());
-                        obj.put("DOB",displayDate.getText().toString());
-                    }catch (JSONException e) {
-                        Toast.makeText(SignUpActivity1.this,"could not make JSON obj", Toast.LENGTH_SHORT).show();
+                    if (currentMonth < selectedMonth || (currentMonth == selectedMonth && currentDay < selectedDay)) {
+                        age--; // Adjust age if the current date is before the selected birthdate
                     }
-                    Requests insert = new Requests(obj,"https://lamp.ms.wits.ac.za/home/s2596852/insertUser.php");
 
-                    String msg = null;
-                    try {
-                        msg = insert.addDataToDatabase();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                    if (age < 13) {
+                        displayDate.setError("You must be 13 years or older");
+
                     }
-                    Toast.makeText(SignUpActivity1.this,msg , Toast.LENGTH_SHORT).show();
+
+
+                    if (!password.equals(confirmPassword)) {
+                        confirmPasswordLayout.setError("Passwords do not match");
+                        Flag = false;
+                    } else if (password.length() < 8) {
+                        passwordLayout.setError("Password must be greater than 8 characters");
+                        Flag = false;
+                    } else if (Flag == true) {
+                        Toast.makeText(SignUpActivity1.this, "Works :D", Toast.LENGTH_SHORT).show();
+                        username.setError(null);
+                        fName.setError(null);
+                        lName.setError(null);
+                        email.setError(null);
+                        displayDate.setError(null);
+                        confirmPasswordLayout.setError(null);
+                        passwordLayout.setError(null);
+                        Flag = true;
+
+
+                        JSONObject obj = new JSONObject();
+                        try {
+                            obj.put("username", username.getText().toString());
+                            obj.put("fname", fName.getText().toString());
+                            obj.put("lname", lName.getText().toString());
+                            obj.put("password", password);
+                            obj.put("email", email.getText().toString());
+                            obj.put("DOB", displayDate.getText().toString());
+                        } catch (JSONException e) {
+                            Toast.makeText(SignUpActivity1.this, "could not make JSON obj", Toast.LENGTH_SHORT).show();
+                        }
+                        Requests insert = new Requests(obj, "https://lamp.ms.wits.ac.za/home/s2596852/insertUser.php");
+
+                        String msg = null;
+                        try {
+                            msg = insert.addDataToDatabase();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        Toast.makeText(SignUpActivity1.this, msg, Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
-        });
-    }
-}
+    });
+
+}}
 
 
 //Liam's code
