c64404e8503735ee1ce7e0566bec39d0a298eeac
zainab <zlorgat1007@gmail.com>
2023-06-03 22:44:03 +0200

    more validations

--- app/src/main/java/com/example/woofer/SignUpActivity1.java
+++ app/src/main/java/com/example/woofer/SignUpActivity1.java
@@ -1,7 +1,7 @@
 package com.example.woofer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Patterns;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
@@ -18,10 +18,6 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.Calendar;
 
 public class SignUpActivity1 extends AppCompatActivity {
@@ -36,20 +32,9 @@ public class SignUpActivity1 extends AppCompatActivity {
 
     boolean isDateSelected = false;
 
-    public String CheckName(String Unam){
 
-        JSONObject n = new JSONObject();
-        try {
-            n.put("username",Unam);
-        }catch (JSONException e){
-            Toast.makeText(SignUpActivity1.this,"could not make JSON n", Toast.LENGTH_SHORT).show();
-        }
-        Requests UniqName = new Requests(n, "https://lamp.ms.wits.ac.za/home/s2596852/checkUser.php");
 
-        return UniqName.getRequest();
-    }
 
-    private static Boolean Flag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,20 +52,20 @@ public class SignUpActivity1 extends AppCompatActivity {
         Button submitButton = findViewById(R.id.newAccConfirmButton);
 
         //for datePicker
-        displayDate= (TextView) findViewById(R.id.textViewDate);
+        displayDate = (TextView) findViewById(R.id.textViewDate);
         displayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar calendar= Calendar.getInstance();
-                int year= calendar.get(Calendar.YEAR);
-                int month= calendar.get(Calendar.MONTH);
-                int day= calendar.get(Calendar.DAY_OF_MONTH);
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog datePicker= new DatePickerDialog(
+                DatePickerDialog datePicker = new DatePickerDialog(
                         SignUpActivity1.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         dateSetListener,
-                        year,month,day);
+                        year, month, day);
                 datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 datePicker.show();
             }
@@ -88,16 +73,16 @@ public class SignUpActivity1 extends AppCompatActivity {
         dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month= month+1;
+                month = month + 1;
                 Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
-                String date=  month +"" + day+"" + year +"";
+                String date = month + "/" + day + "/" + year;
                 displayDate.setText(date);
 
                 isDateSelected = true;
             }
         };
         //Return to login page
-        Button backButton = (Button)findViewById(R.id.newAccBackButton);
+        Button backButton = (Button) findViewById(R.id.newAccBackButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,104 +92,123 @@ public class SignUpActivity1 extends AppCompatActivity {
         });
 
         //Validation checks
-        username=findViewById(R.id.uNameNewAccView);
-        fName=findViewById(R.id.fNameNewAccView);
-        lName=findViewById(R.id.lNameNewAccView);
-        email=findViewById(R.id.editTextEmail);
-        confirm= findViewById(R.id.newAccConfirmButton);
-
-        String Uname = username.getText().toString();
+        username = findViewById(R.id.uNameNewAccView);
+        fName = findViewById(R.id.fNameNewAccView);
+        lName = findViewById(R.id.lNameNewAccView);
+        email = findViewById(R.id.editTextEmail);
+        confirm = findViewById(R.id.newAccConfirmButton);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                String Uname = username.getText().toString();
-                String Resp = CheckName(Uname);
-                if (Resp.equals("Username already exists")){
-                    Toast.makeText(SignUpActivity1.this,"Username taken, please try again", Toast.LENGTH_SHORT).show();
-                    username.setError("Choose a different Username");
-                }
-                else if (Resp.equals("Username does not exist")){
-                    Flag = true;
-                }
-
                 String password = passwordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
+                String firstNameText = fName.getText().toString();
+                String lastNameText = lName.getText().toString();
+                String emailtext = email.getText().toString();
+                String usernameText = username.getText().toString();
 
-                if (username.length()==0){
+                if (username.length() == 0) {
                     username.setError("Enter username");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
-                    Flag = false;
                 }
-                else if (fName.length()==0) {
+//NEED TO CHECK UNIQUENESS
+
+                else if (usernameText.length() < 4) {// Check if the username is at least 4 characters long
+                    username.setError("Username must be at least 4 characters long");
+                    Toast.makeText(SignUpActivity1.this, "Username must be at least 4 characters long", Toast.LENGTH_SHORT).show();
+
+                }
+
+                else if (!usernameText.matches("[a-zA-Z0-9_]+")) { // Check if the username contains only alphanumeric characters and underscores
+                    username.setError("Username can only contain letters, numbers, and underscores");
+                    Toast.makeText(SignUpActivity1.this, "Username can only contain letters, numbers, and underscores", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (fName.length() == 0) {
                     fName.setError("Enter first name");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
-                    Flag = false;
                 }
-                else if (lName.length()==0) {
+
+                else if (lName.length() == 0) {
                     lName.setError("Enter last name");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
-                    Flag = false;
                 }
-                else if (email.length()==0) {
+
+                else if (!firstNameText.matches("[a-zA-Z]+")) {
+                    fName.setError("Enter a valid first name");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid first name", Toast.LENGTH_SHORT).show();
+                     // Check if the first name contains only letters
+                }
+
+                else if (!lastNameText.matches("[a-zA-Z]+")) {
+                    lName.setError("Enter a valid last name");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid last name", Toast.LENGTH_SHORT).show();
+                    // Check if the last name contains only letters
+                }
+
+                else if (email.length() == 0) {
                     email.setError("Enter email");
                     Toast.makeText(SignUpActivity1.this, "Please fill in required fields", Toast.LENGTH_SHORT).show();
-                    Flag = false;
                 }
+
+                else if (!Patterns.EMAIL_ADDRESS.matcher(emailtext).matches()) { //email is valid format
+                    email.setError("Enter email");
+                    Toast.makeText(SignUpActivity1.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+                }
+
                 else if (!isDateSelected) {
                     // Date is not selected, show error message
                     displayDate.setError("Please select a date");
-                    Flag = false;
-                }
-                else if (!password.equals(confirmPassword)) {
-                    confirmPasswordLayout.setError("Passwords do not match");
-                    Flag = false;
-                }
-                else if (password.length() < 8) {
-                    passwordLayout.setError("Password must be greater than 8 characters");
-                    Flag = false;
                 }
 
-                else if (Flag == true){
-                    Toast.makeText(SignUpActivity1.this, "Works :D", Toast.LENGTH_SHORT).show();
-                    username.setError(null);
-                    fName.setError(null);
-                    lName.setError(null);
-                    email.setError(null);
-                    displayDate.setError(null);
-                    confirmPasswordLayout.setError(null);
-                    passwordLayout.setError(null);
-                    Flag = true;
+                else {
+                    // Age check
+                    Calendar calendar = Calendar.getInstance();
+                    int currentYear = calendar.get(Calendar.YEAR);
+                    int currentMonth = calendar.get(Calendar.MONTH) + 1;
+                    int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
 
+                    String[] selectedDateParts = displayDate.getText().toString().split("/");
+                    int selectedMonth = Integer.parseInt(selectedDateParts[0]);
+                    int selectedDay = Integer.parseInt(selectedDateParts[1]);
+                    int selectedYear = Integer.parseInt(selectedDateParts[2]);
 
-                    JSONObject obj = new JSONObject();
-                    try{
-                        obj.put("username",username.getText().toString());
-                        obj.put("fname",fName.getText().toString());
-                        obj.put("lname",lName.getText().toString());
-                        obj.put("password",password);
-                        obj.put("email",email.getText().toString());
-                        obj.put("DOB",displayDate.getText().toString());
-                    }catch (JSONException e) {
-                        Toast.makeText(SignUpActivity1.this,"could not make JSON obj", Toast.LENGTH_SHORT).show();
+                    int age = currentYear - selectedYear;
+
+                    if (currentMonth < selectedMonth || (currentMonth == selectedMonth && currentDay < selectedDay)) {
+                        age--; // Adjust age if the current date is before the selected birthdate
                     }
-                    Requests insert = new Requests(obj,"https://lamp.ms.wits.ac.za/home/s2596852/insertUser.php");
 
-                    String msg = null;
-                    try {
-                        msg = insert.addDataToDatabase();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                    if (age < 13) {
+                        displayDate.setError("You must be 13 years or older");
+                        return;
                     }
-                    Toast.makeText(SignUpActivity1.this,msg , Toast.LENGTH_SHORT).show();
+
+
+                    if (!password.equals(confirmPassword)) {
+                        confirmPasswordLayout.setError("Passwords do not match");
+                    }
+
+                    else if (password.length() < 8) {
+                        passwordLayout.setError("Password must be 8 characters or more");
+                    }
+
+                    else {
+                        Toast.makeText(SignUpActivity1.this, "Works :D", Toast.LENGTH_SHORT).show();
+                        username.setError(null);
+                        fName.setError(null);
+                        lName.setError(null);
+                        email.setError(null);
+                        displayDate.setError(null);
+                        confirmPasswordLayout.setError(null);
+                        passwordLayout.setError(null);
+                    }
                 }
             }
         });
-    }
-}
 
-
+    }}
 //Liam's code
 /*
 SignUpActivity1.this.runOnUiThread(new Runnable() {
